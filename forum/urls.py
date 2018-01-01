0de901ec562e2dfa1d6289eaca0d7035a83d156c
BravoLu <260484443@qq.com>
2018-01-01 14:04:45 +0800

    finish forum

--- forum/urls.py
+++ forum/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url
 from forum import views
 
 urlpatterns = [
-    url(r'^$',views.articles,name="create_forums"),
+	url(r'^M$', views.math,name="math"),
+	url(r'^E$',views.foreign,name="foreign"),
+    url(r'^C$',views.articles,name="create_forums"),
     url(r'^(?P<slug>[-\w]+)/$', views.article, name='article'),
-    url(r'^write$', views.CreateArticle, name='write'),
+    url(r'^[A-Z]/write/$', views.CreateArticle, name='write'),
     url(r'^comment', views.comment, name='comment'),
 ]  
