0b72868662e8ed33a518a7cfaffd3ded6391a184
麦文俊 <429839446@qq.com>
2018-01-21 19:35:47 +0800

    Update celery.py
    No desc

--- LemonTea/celery.py
+++ LemonTea/celery.py
@@ -6,16 +6,16 @@ from LemonTea.settings import DEBUG
 if not DEBUG:
     from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LemonTea.settings')
+    # set the default Django settings module for the 'celery' program.
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LemonTea.settings')
 
-app = Celery('LemonTea')
+    app = Celery('LemonTea')
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+    # Using a string here means the worker will not have to
+    # pickle the object when using Windows.
+    app.config_from_object('django.conf:settings')
+    app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+    @app.task(bind=True)
+    def debug_task(self):
+        print('Request: {0!r}'.format(self.request))
